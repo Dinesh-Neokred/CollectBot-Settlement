32807b9afe614c6d9a0d9e9cf283894ffb1f26be
Dinesh-Neokred <122446642+Dinesh-Neokred@users.noreply.github.com>
2024-01-23 14:30:47 +0530

    Auth token Added and removed unused code

--- CollectBot-Settlement/src/test/java/settlement/ReadAndWriteBeforeSettlememtBalance.java
+++ CollectBot-Settlement/src/test/java/settlement/ReadAndWriteBeforeSettlememtBalance.java
@@ -20,43 +20,26 @@ public class ReadAndWriteBeforeSettlememtBalance extends login {
         public static String baseUrl = "https://collectbot.neokred.tech/core-svc/api/v1/";
 
         @Test
-        public double getBeforeDebitBalance(String clientId)
+        public double getBeforeDebitBalance(String clientId, String token)
                         throws EncryptedDocumentException, IOException {
-                String auth = getAuth("admin@neokred.tech", "Neokred@12345");
 
                 RequestSpecification requestPayload = given().contentType("application/json")
-                                .header("Authorization", auth)
+                                .header("Authorization", token)
                                 .header("userid", clientId);
                 double balance = requestPayload.when().get(baseUrl + "service/client/balance").jsonPath()
                                 .getDouble("data.debited");
                 return balance;
-
-                // FileInputStream fis = new FileInputStream("./data/Settlement.xlsx");
-                // Workbook book = WorkbookFactory.create(fis);
-                // Sheet data = book.getSheet("userID");
-                // data.getRow(1 + rowNumber).getCell(15).setCellValue(balance);
-                // FileOutputStream fos = new FileOutputStream("./data/Settlement.xlsx");
-                // book.write(fos);
-                // book.close();
 
         }
 
         @Test()
-        public double getAfterDebitBalance(String clientId)
+        public double getAfterDebitBalance(String clientId, String token)
                         throws EncryptedDocumentException, IOException {
                 RequestSpecification requestPayload = given().contentType("application/json")
-                                .header("Authorization", getAuth("admin@neokred.tech", "Neokred@12345"))
+                                .header("Authorization", token)
                                 .header("userid", clientId);
                 double balance = requestPayload.when().get(baseUrl + "service/client/balance").jsonPath()
                                 .getDouble("data.debited");
                 return balance;
-                // FileInputStream fis = new FileInputStream("./data/Settlement.xlsx");
-                // Workbook book = WorkbookFactory.create(fis);
-                // Sheet data = book.getSheet("userID");
-                // data.getRow(1 + rowNumber).getCell(16).setCellValue(balance);
-                // FileOutputStream fos = new FileOutputStream("./data/Settlement.xlsx");
-                // book.write(fos);
-                // book.close();
-
         }
 }
