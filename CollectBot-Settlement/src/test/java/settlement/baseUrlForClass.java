84e4667d7c98ea511d9ff8b1c75586ab76784b64
Dinesh-Neokred <122446642+Dinesh-Neokred@users.noreply.github.com>
2024-03-14 11:56:51 +0530

    base Url class Added

---
+++ CollectBot-Settlement/src/test/java/settlement/baseUrlForClass.java
@@ -0,0 +1,8 @@
+package settlement;
+
+public class baseUrlForClass {
+
+    String coreBaseUrl = "https://collectbot.neokred.tech/core-svc/api/v1/";
+    String userBaseUrl = "https://collectbot.neokred.tech/user-svc/api/v1/";
+
+}
