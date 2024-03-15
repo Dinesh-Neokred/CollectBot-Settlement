630548dda544c7c9a31745c0da74f78b575aa3cc
Dinesh-Neokred <122446642+Dinesh-Neokred@users.noreply.github.com>
2024-03-15 09:59:35 +0530

    added qa base url

--- CollectBot-Settlement/src/test/java/settlement/baseUrlForClass.java
+++ CollectBot-Settlement/src/test/java/settlement/baseUrlForClass.java
@@ -2,7 +2,10 @@ package settlement;
 
 public class baseUrlForClass {
 
-    String coreBaseUrl = "https://collectbot.neokred.tech/core-svc/api/v1/";
-    String userBaseUrl = "https://collectbot.neokred.tech/user-svc/api/v1/";
+    String coreBaseUrl = "http://qa-collectbot.neokred.tech:9080/core-svc/api/v1/";
+    String userBaseUrl = "http://qa-collectbot.neokred.tech:9080/user-svc/api/v1/";
+
+    // String coreBaseUrl = "https://collectbot.neokred.tech/core-svc/api/v1/";
+    // String userBaseUrl = "https://collectbot.neokred.tech/user-svc/api/v1/";
 
 }
