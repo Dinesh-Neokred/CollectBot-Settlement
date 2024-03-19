c2c0c96c1facc45013e8c7c6011d188edf3613cd
Dinesh-Neokred <122446642+Dinesh-Neokred@users.noreply.github.com>
2024-03-19 15:09:59 +0530

    chnages

--- CollectBot-Settlement/src/test/java/settlement/baseUrlForClass.java
+++ CollectBot-Settlement/src/test/java/settlement/baseUrlForClass.java
@@ -14,7 +14,9 @@ public class BaseUrlForClass {
     // String userBaseUrl =
     // "http://qa-collectbot.neokred.tech:9080/user-svc/api/v1/";
 
-    String coreBaseUrl = "https://collectbot.neokred.tech/core-svc/api/v1/";
-    String userBaseUrl = "https://collectbot.neokred.tech/user-svc/api/v1/";
+    // String coreBaseUrl = "https://collectbot.neokred.tech/core-svc/api/v1/";
+    // String userBaseUrl = "https://collectbot.neokred.tech/user-svc/api/v1/";
+    String coreBaseUrl = "https://preprod-collectbot.neokred.tech/core-svc/api/v1/";
+    String userBaseUrl = "https://preprod-collectbot.neokred.tech/user-svc/api/v1/";
 
 }
